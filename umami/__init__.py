2dbfe84b69ae74fe5d0b9978a1766be5e929299b
Katherine Barnhart <katy.barnhart@gmail.com>
2019-07-22 07:57:14 -0600

    formatting

--- umami/__init__.py
+++ umami/__init__.py
@@ -1,8 +1,8 @@
+from ._version import get_versions
 from .metric import Metric
 from .residual import Residual
 
 __all__ = ["Metric", "Residual"]
 
-from ._version import get_versions
-__version__ = get_versions()['version']
+__version__ = get_versions()["version"]
 del get_versions
